0fc801ba57659bf321861492ef3bdf7fb0079db6
hzlshen <m15277968105@163.com>
2017-10-19 11:51:42 +0800

    display data 完成

--- display-data/src/app/app.component.ts
+++ display-data/src/app/app.component.ts
@@ -1,7 +1,39 @@
 import { Component } from '@angular/core';
+import {Hero} from "./hero";
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>{{title}}</h1>
+<h2>My favorite hero is: {{myHero.name}}</h2>
+<p>Heroes:</p>
+<ul>
+  <li *ngFor="let hero of heroes">
+  {{ hero.name }}
+</li>
+</ul>
+<p *ngIf="heroes.length > 3">There are many heroes!</p>
+`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  //变量赋值
+  title = "Tour of Heroes";
+  //myHero = "Windstorm";
+  heroes =[
+    new Hero(1,"Windstorm"),
+    new Hero(2,"Bombasto"),
+    new Hero(3,'Magneta'),
+    new Hero(24,'Tornado')
+  ];
+  
+  //使用构造函数声明和初始化属性
+  /*title:string;
+  myHero:string;
+  
+  constructor(){
+    this.title = "Tour of Heroes";
+    this.myHero = "Windstorm";
+  }*/
+  //两种风格的效果是一样的
+  myHero = this.heroes[0];
+}
---
+++ display-data/src/app/hero.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/10/19.
+ */
+export class Hero{
+  constructor(
+    public id :number,
+    public name :string
+  ){}
+}
