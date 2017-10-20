84f1f9f6897a5da7f16cd5fc8a28b246b96c537d
hzlshen <m15277968105@163.com>
2017-10-20 16:36:32 +0800

    用户输入完成

--- ngForm/src/app/app.component.ts
+++ ngForm/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  clickMessage='';
+  values = '';
+
+  onClickMe(){
+    this.clickMessage = "You are my hero!";
+  }
+  
+  onKey(event:any){
+    this.values += event.target.value+'|';
+  }
+  value = '';
+  onEnter(value: string) { this.value = value; }
+  
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  addHero(newHero: string) {
+    if (newHero) {
+      this.heroes.push(newHero);
+    }
+  }
 }
