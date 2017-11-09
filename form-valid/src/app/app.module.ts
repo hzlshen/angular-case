8e221dfb1f365a55af0c11ec196a343df5c39850
hzlshen <m15277968105@163.com>
2017-11-09 17:16:38 +0800

    引入模块

--- form-valid/src/app/app.module.ts
+++ form-valid/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClickMeComponent } from './click-me.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClickMeComponent,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ form-valid/src/app/click-me.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-click-me",
+  templateUrl: "./click-me.component.html",
+  styleUrls: ["./click-me.component.scss"]
+})
+
+export class ClickMeComponent implements OnInit {
+  
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+
+  }
+}
