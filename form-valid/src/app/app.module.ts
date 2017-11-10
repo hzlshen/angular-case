6f8a22281e8fa7dd067c44a7bfd1da39b1b2213e
hzlshen <m15277968105@163.com>
2017-11-10 10:34:56 +0800

    把多余的demo component删了 重写form表单组件

--- form-valid/src/app/app.module.ts
+++ form-valid/src/app/app.module.ts
@@ -1,24 +1,20 @@
-import { KeyupComponent } from './keyup.component';
+import { HeroFormComponent } from './hero-form.component';
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ClickMeComponent } from './click-me.component';
 import {FormsModule} from '@angular/forms';
-// import { HeroComponent } from './hero/hero.component';
-import { HeroFormComponent } from './hero/hero-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClickMeComponent,
-    KeyupComponent,
-    // HeroComponent,
-
+    HeroFormComponent
   ],
   imports: [
     BrowserModule,
-    HeroFormComponent,
     FormsModule
   ],
   providers: [],
