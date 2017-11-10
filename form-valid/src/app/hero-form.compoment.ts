e9753962ebc8f4150ffa36e6ca42e66f8aa4cf58
hzlshen <m15277968105@163.com>
2017-11-10 10:42:23 +0800

    hero-formComponent

--- form-valid/src/app/hero-form.compoment.ts
+++ form-valid/src/app/hero-form.compoment.ts
@@ -1,2 +1,24 @@
-
 import { Component } from '@angular/core';
+
+import { Hero } from './hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html'
+ })
+
+ export class HeroFormComponent {
+
+   powers = ['Really Smart', 'Super Flexible',
+             'Super Hot', 'Weather Changer'];
+
+   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+   submitted = false;
+
+   onSubmit() { this.submitted = true; }
+
+   newHero() {
+     this.model = new Hero(42, '', '');
+   }
+ }
